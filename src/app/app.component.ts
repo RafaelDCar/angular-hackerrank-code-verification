5bae29da9061ef6511204e774b0dc844872ac4e7
HackerRank <hackerrank@example.com>
2019-01-22 14:11:19 +0000

    Saved at Tue Jan 22 2019 12:11:19 GMT-0200 (Horário de Verão de Brasília)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -49,6 +49,6 @@ export class AppComponent {
 
   submitForm() {
     /* Change the display message on button click / submit form */
-    if(this.paymentForm.isva)
+    if(this.paymentForm.)
   }
 }
