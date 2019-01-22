50dfbfb267b7079b3eee2a2f4aea7bb7c78cec0c
HackerRank <hackerrank@example.com>
2019-01-22 17:09:50 +0000

    Saved at Tue Jan 22 2019 15:09:50 GMT-0200 (Horário de Verão de Brasília)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -54,7 +54,7 @@ export class AppComponent {
     this.paymentForm.controls['expirationYear'].setValue("2017");
     this.paymentForm.controls['cvv'].setValue("677");
     /* Change the display message on button click / submit form */
-    console.log('this.paymentForm.errors);
+    console.log('Erros'this.paymentForm.errors);
     if(this.paymentForm.valid){
         this.displayMessage = 'Payment Successful!'
     } else {
