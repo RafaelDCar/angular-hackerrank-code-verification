f356c4876d0dd2e73ee8967e2ee96d08d2c8dc9c
HackerRank <hackerrank@example.com>
2019-01-22 14:19:19 +0000

    Saved at Tue Jan 22 2019 12:19:18 GMT-0200 (Horário de Verão de Brasília)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,7 +19,7 @@ export class AppComponent {
   
   creatForm() {
       this.paymentForm = this.formBuilder.group({
-          nameOnCard: ['', [Validators.requiValidators.pattern(new RegExp('[A-Za-z]'))]],
+          nameOnCard: ['', [Validators.required, Validators.pattern(new RegExp('[A-Za-z]'))]],
           cardNumber: ['', Validators.pattern(new RegExp('[0-1]{16}'))],
           expirationMonth: ['', Validators.pattern(new RegExp('[0-1]{2}'))],
           expirationYear: ['', Validators.pattern(new RegExp('[0-1]{4}'))],
