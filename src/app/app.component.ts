7d154f031b7927e5ab43f474f4d4918a0939681a
HackerRank <hackerrank@example.com>
2019-01-22 16:26:51 +0000

    Saved at Tue Jan 22 2019 14:26:50 GMT-0200 (Horário de Verão de Brasília)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -49,7 +49,7 @@ export class AppComponent {
 
   submitForm() {
     /* Change the display message on button click / submit form */
-    console.log(t)
+    console.log(this)
     if(this.paymentForm.valid){
         this.displayMessage = 'Payment Successful!'
     } else {
