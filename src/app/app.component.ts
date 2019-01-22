97de3a72aba163e3679f0b79e5cfaef628dabfe8
HackerRank <hackerrank@example.com>
2019-01-22 13:30:39 +0000

    Saved at Tue Jan 22 2019 11:30:39 GMT-0200 (Horário de Verão de Brasília)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,7 +18,9 @@ export class AppComponent {
   }
   
   creatForm() {
-      this.paymentForm = this.formBuilder.group()
+      this.paymentForm = this.formBuilder.group({
+          
+      })
   }
 
   submitForm() {
