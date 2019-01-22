98d05bd806fd679b4c50075802909ed655dba18f
HackerRank <hackerrank@example.com>
2019-01-22 16:37:54 +0000

    Saved at Tue Jan 22 2019 14:37:54 GMT-0200 (Horário de Verão de Brasília)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -50,7 +50,7 @@ export class AppComponent {
   submitForm() {
       this.paymentForm.controls['nameOnCard'].setValue("Mathew Jerome");
     this.paymentForm.controls['cardNumber'].setValue("3456234567854567");
-    app.paymentForm.controls['expirationMonth'].setValue("11");
+    this.paymentForm.controls['expirationMonth'].setValue("11");
     app.paymentForm.controls['expirationYear'].setValue("2017");
     app.paymentForm.controls['cvv'].setValue("677");
     /* Change the display message on button click / submit form */
