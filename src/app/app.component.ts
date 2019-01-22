6a630e0bdea1babd584d2b6155c95712e992dc24
HackerRank <hackerrank@example.com>
2019-01-22 17:09:54 +0000

    Saved at Tue Jan 22 2019 15:09:54 GMT-0200 (Horário de Verão de Brasília)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -55,6 +55,7 @@ export class AppComponent {
     this.paymentForm.controls['cvv'].setValue("677");
     /* Change the display message on button click / submit form */
     console.log('Erros',this.paymentForm.errors);
+    console.log('Erros',this.paymentForm.errors);
     if(this.paymentForm.valid){
         this.displayMessage = 'Payment Successful!'
     } else {
