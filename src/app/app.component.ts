679c1e6652e0ee61a4cf2e8173fdc17dfe1ca3a8
HackerRank <hackerrank@example.com>
2019-01-22 13:28:39 +0000

    Saved at Tue Jan 22 2019 11:28:38 GMT-0200 (Horário de Verão de Brasília)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
   templateUrl: './app.component.html'
 })
 
-export class AppComponent implements O {
+export class AppComponent implements On {
   paymentForm: FormGroup;
   displayMessage: string;
 
