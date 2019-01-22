50eb7f3b30727490191e322b77db2d76af3cf4bf
HackerRank <hackerrank@example.com>
2019-01-22 13:31:26 +0000

    Saved at Tue Jan 22 2019 11:31:26 GMT-0200 (Horário de Verão de Brasília)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,7 +20,8 @@ export class AppComponent {
   creatForm() {
       this.paymentForm = this.formBuilder.group({
           name: [],
-          cardNumber: []
+          cardNumber: [],
+          
       })
   }
 
