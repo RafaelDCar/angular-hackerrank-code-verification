50e38320b21979838ba9afac28437b56b48d711b
HackerRank <hackerrank@example.com>
2019-01-22 14:00:21 +0000

    Saved at Tue Jan 22 2019 12:00:20 GMT-0200 (Horário de Verão de Brasília)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,7 +19,7 @@ export class AppComponent {
   
   creatForm() {
       this.paymentForm = this.formBuilder.group({
-          name: ['', Validators.pattern(new Regex('[A-Za-z]'))],
+          name: ['', Validators.pattern(new ('[A-Za-z]'))],
           cardNumber: ['', [Validators.minLength(16), Validators.maxLength(16)]],
           expirationMonth: ['', [Validators.minLength(2), Validators.maxLength(2)]],
           expirationYear: ['', [Validators.minLength(4), Validators.maxLength(4)]],
