ccef84b8539c0f8b02e043a7d7df53fa88d7b1d6
HackerRank <hackerrank@example.com>
2019-01-22 17:11:27 +0000

    Saved at Tue Jan 22 2019 15:11:27 GMT-0200 (Horário de Verão de Brasília)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -56,7 +56,7 @@ export class AppComponent {
     /* Change the display message on button click / submit form */
     console.log('Erros',this.paymentForm.errors);
     console.log('Valid',this.paymentForm.valid);
-    this.paymentForm.valid = true;
+    this.paymentForm. = true;
     if(this.paymentForm.valid === true){
         this.displayMessage = 'Payment Successful!'
     } else {
