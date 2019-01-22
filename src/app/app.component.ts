026eff128799312d31b9d2eb4b00aefb3a6c5b96
HackerRank <hackerrank@example.com>
2019-01-22 13:45:53 +0000

    Saved at Tue Jan 22 2019 11:45:52 GMT-0200 (Horário de Verão de Brasília)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -27,7 +27,9 @@ export class AppComponent {
       })
   }
   
-  get name() 
+  get name() {
+      ret
+  }
 
   submitForm() {
     /* Change the display message on button click / submit form */
